af8bc1237af04421da7901a32d6fc7b29bd379db
mantuk87 <mantu.montu1987@gmail.com>
2021-01-20 07:53:57 +0530

    First commit on 20 Jan 21

---
+++ src/main/java/com/qa/utils/TestUtils.java
@@ -0,0 +1,8 @@
+package com.qa.utils;
+
+public class TestUtils {
+	public void testUtils() {
+		System.out.println("TestUtils Page method");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,23 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void test1Test() {
+		System.out.println("Test1 from LoginTest TestNG class");
+		System.out.println("hello");
+	}
+	@Test
+	public void test2Test() {
+		System.out.println("Test2 from LoginTest TestNG class");		
+	}
+	
+	@Test
+	public void test3Test() {
+		System.out.println("Added test from GIT itself");
+		System.out.println("New line added -, from GIT itself, modfied from eclipse");
+	}
+
+}
